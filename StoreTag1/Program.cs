c28a5a470c0afb4ec6c5cb29ce751cc32352a032
luckybluejay27 <luckybluejay27@gmail.com>
2024-04-23 00:21:18 -0700

    Basis BS

--- StoreTag1/Program.cs
+++ StoreTag1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+public class Program
+{
+    
+    public static void Main()
+    {
+        //prompt user for new data
+        Console.WriteLine("Please Enter New Data");
+        string? newData = Console.ReadLine();
+
+        //read data into object
+        
+        //add data to TagStore.json
+    }
+}
--- StoreTag1/obj/Debug/net8.0/StoreTag1.AssemblyInfo.cs
+++ StoreTag1/obj/Debug/net8.0/StoreTag1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b60081f7090d7350458fce933e9cd0f72b4b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73df0cff791cd660fb7fef156ec0e2bb4014a4be")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
