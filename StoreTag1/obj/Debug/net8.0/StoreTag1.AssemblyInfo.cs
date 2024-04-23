36b60081f7090d7350458fce933e9cd0f72b4b54
luckybluejay27 <luckybluejay27@gmail.com>
2024-04-22 23:09:07 -0700

    Missing INIT Tools

--- StoreTag1/obj/Debug/net8.0/StoreTag1.AssemblyInfo.cs
+++ StoreTag1/obj/Debug/net8.0/StoreTag1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa98d9e0519eb7f77d91bdfd36c0686f8fb2d9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30da8dd545e62110f1fac42873b783051ed65c52")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreTag1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
